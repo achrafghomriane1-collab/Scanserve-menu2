418351da5c66b12ec96b6fb49c8c95c8af8ab4ba
achrafghomriane1-collab <achrafghomriane1@gmail.com>
2026-07-11 03:16:54 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,82 @@
+const MenuDatabase = {
+    "ENTRÉES FROIDES": [
+        { id: "ef1", name: "Salade César", price: 700 },
+        { id: "ef2", name: "Salade Variée", price: 600 },
+        { id: "ef3", name: "Salade au Thon", price: 700 },
+        { id: "ef4", name: "Calamars Frits", price: 1500 },
+        { id: "ef5", name: "Scampi De Crevettes", price: 1100 }
+    ],
+    "ENTRÉES CHAUDES": [
+        { id: "eh1", name: "Soupe De Poisson", price: 600 },
+        { id: "eh2", name: "Gratin Poulet", price: 500 },
+        { id: "eh3", name: "Gratin Fruit De Mer", price: 600 },
+        { id: "eh4", name: "Omelette Crevettes", price: 800 },
+        { id: "eh5", name: "Beignets De Camembert", price: 600 }
+    ],
+    "POULETS": [
+        { id: "p1", name: "Escalope à La Créme", price: 1200, bestSeller: true },
+        { id: "p2", name: "Vol Au Vent Sauce Pêcheur", price: 1400 },
+        { id: "p3", name: "Escalope Chiken Curry", price: 1100 },
+        { id: "p4", name: "Escalope Grillé", price: 900 },
+        { id: "p5", name: "Cordan Bleu", price: 1100 },
+        { id: "p6", name: "Wok De Poulet Façon thaï", price: 1200 }
+    ],
+    "SPÉCIALITÉ TUNISIENNE": [
+        { id: "st1", name: "Ojja Fruits De Mer", price: 1500 },
+        { id: "st2", name: "Ojjà Merguez", price: 1200 }
+    ],
+    "GRILLADES": [
+        { id: "g1", name: "Filet De Boeuf Griller", price: 2200 },
+        { id: "g2", name: "Entre Côte Grillée", price: 1900 },
+        { id: "g3", name: "Côtelettes Grillées", price: 1900 },
+        { id: "g4", name: "Noisettes Grillées", price: 1800 },
+        { id: "g5", name: "Merguez Grillée", price: 1300 },
+        { id: "g6", name: "Mixte Grillade", price: 4500, bestSeller: true }
+    ],
+    "PÂTES": [
+        { id: "pa1", name: "Bolognaise", price: 1100 },
+        { id: "pa2", name: "Putanesca", price: 900 },
+        { id: "pa3", name: "4 Fromages", price: 1400 },
+        { id: "pa4", name: "Pasta Alfredo", price: 1100 },
+        { id: "pa5", name: "Crevette Sauce Pesto", price: 1600 },
+        { id: "pa6", name: "Pasta Fruit De Mer", price: 1800 }
+    ],
+    "PAELLA": [
+        { id: "pl1", name: "Paella Dimarisco", price: 2500 },
+        { id: "pl2", name: "Paella Valencienne", price: 3000 }
+    ],
+    "PIZZAS": [
+        { id: "pz1", name: "Pizza Végétarienne", price: 700 },
+        { id: "pz2", name: "Pizza Margherita", price: 600 },
+        { id: "pz3", name: "Pizza Napolitaine", price: 700 },
+        { id: "pz4", name: "Pizza Poulet", price: 800 },
+        { id: "pz5", name: "Pizza Algéroise", price: 700 },
+        { id: "pz6", name: "Pizza Thon", price: 800 },
+        { id: "pz7", name: "Pizza Viande Hachée", price: 800 },
+        { id: "pz8", name: "Pizza Alpina", price: 900 },
+        { id: "pz9", name: "Pizza Capricieuse", price: 800 },
+        { id: "pz10", name: "Pizza Sauce Pesto", price: 1000 },
+        { id: "pz11", name: "Pizza 4 Fromages", price: 1200 },
+        { id: "pz12", name: "Pizza Fruits De Mer", price: 1400 }
+    ],
+    "POISSONS": [
+        { id: "po1", name: "Crevettes Sauté À L'ail", price: 2200 },
+        { id: "po2", name: "Crevettes En Sauce", price: 2200 },
+        { id: "po3", name: "Calamares En Sauce", price: 1800 },
+        { id: "po4", name: "Dorade Grillée", price: 2200 },
+        { id: "po5", name: "Loup Grillée", price: 2200 },
+        { id: "po6", name: "Espadon Sauce Normande", price: 2600 },
+        { id: "po7", name: "Espadon Sauce Meunière", price: 2400 },
+        { id: "po8", name: "Crevettes Grillée", price: 2200 }
+    ],
+    "DESSERTS & BOISSONS": [
+        { id: "d1", name: "Tiramisu", price: 400 },
+        { id: "d2", name: "Fondant au Chocolat", price: 400 },
+        { id: "d3", name: "Mousse au Chocolat", price: 400 },
+        { id: "d4", name: "Crème Brûlée", price: 400 },
+        { id: "d5", name: "Soda", price: 200 },
+        { id: "d6", name: "Jus Nature", price: 400 },
+        { id: "d7", name: "Virgin Mojito", price: 500 },
+        { id: "d8", name: "Café / Thé", price: 150 }
+    ]
+};
